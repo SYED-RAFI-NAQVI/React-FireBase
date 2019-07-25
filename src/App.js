b36b38d31f75446bb4a278464f72ff6357de6ade
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2019-07-25 20:12:34 +0530

    trying to to add

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 function App () {
   const [state, setState] = useState({
     name : '',
-    isToggleOn : false,
   });
   
 
@@ -14,24 +13,16 @@ function App () {
     setState(
       {
         name:e.target.value,
-        isToggleOn : false,
       }
     )
   }
-  let show = null
 
-if(state.isToggleOn === false) {
-  show = (
-  <p>{state.name}</p>
-  )
-}
   
 // Toggling button 
 const buttonHandler = (e) => {
   e.preventDefault();
   setState(state => ({
     name: state.name,
-    isToggleOn: !state.isToggleOn
   }));
 }
 
@@ -41,7 +32,7 @@ const buttonHandler = (e) => {
         <h1>FireBase</h1>
         <input className = "int" onChange = {inputHandler}/>
         <button className = "btn" onClick = {buttonHandler}>submit</button>
-        {show}
+        <p>{state.name}</p>
       </header>
     </div>
   );
